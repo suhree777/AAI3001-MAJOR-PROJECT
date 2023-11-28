d6c92324321678992787df5444f33c62cac3f4e7
Seri <serihaniffah@gmail.com>
2023-11-28 10:40:55 +0800

    changed data_directory to data_path , added save_path
    save_path save all the figures that has been annotated in MoNuSegDataset file

--- dataprep.py
+++ dataprep.py
@@ -1,7 +1,6 @@
 import os
 import cv2
 import xml.etree.ElementTree as ET
-import matplotlib.pyplot as plt
 import numpy as np
 
 def parse_xml(xml_path):
@@ -33,13 +32,29 @@ def visualize_annotations(image_path, annotations):
         cv2.polylines(image, [pts], isClosed=True, color=(0, 255, 255), thickness=2)
 
     # Display the image with bounding boxes
-    plt.imshow(image[:, :, ::-1])  # Convert BGR to RGB
-    plt.title(image_path)
-    plt.show()
+    cv2.imshow("Visualized Image", image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-def load_monuseg_dataset(data_dir):
-    image_dir = os.path.join(data_dir, 'Tissue Images')
-    annotation_dir = os.path.join(data_dir, 'Annotations')
+# to save item in a new folder
+def save_visualized_image(image_path, annotations, save_path):
+    # Read image
+    image = cv2.imread(image_path)
+
+    # Draw bounding boxes on the image
+    for vertices in annotations:
+        pts = np.array(vertices, np.int32)
+        pts = pts.reshape((-1, 1, 2))
+        cv2.polylines(image, [pts], isClosed=True, color=(0, 255, 255), thickness=2)
+
+    # Save the image with bounding boxes
+    filename = os.path.basename(image_path)
+    save_path = os.path.join(save_path, f"visualized_{os.path.splitext(filename)[0]}_{len(annotations)}.png")
+    cv2.imwrite(save_path, image)
+
+def load_monuseg_dataset(data_path, save_path=None):
+    image_dir = os.path.join(data_path, 'Tissue Images')
+    annotation_dir = os.path.join(data_path, 'Annotations')
 
     image_files = [f for f in os.listdir(image_dir) if f.endswith('.tif')]
 
@@ -51,10 +66,15 @@ def load_monuseg_dataset(data_dir):
         if os.path.exists(annotation_path):
             annotations = parse_xml(annotation_path)
             visualize_annotations(image_path, annotations)
+
+            # Save the visualized image to the MoNuSegDataset directory
+            if save_path:
+                save_visualized_image(image_path, annotations, save_path)
         else:
             print(f"Warning: Annotation file not found for image {image_path}.")
-            
-            
-# File Directory, change accordingly
-data_directory = r'C:\Users\serih\Desktop\AAI3001-MAJOR-PROJECT\MoNuSeg 2018 Training Data'
-load_monuseg_dataset(data_directory)
+
+# File Path, change accordingly
+data_path = 'MoNuSeg 2018 Training Data'
+save_path = 'MoNuSegDataset'
+
+load_monuseg_dataset(data_path, save_path)
