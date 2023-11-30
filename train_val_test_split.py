88d123ee56ff6c8b0a42a6680f839d65e5cfc1a4
2201773Supraja <143372165+2201773Supraja@users.noreply.github.com>
2023-11-30 14:31:02 +0800

    Update train_val_test_split.py

--- train_val_test_split.py
+++ train_val_test_split.py
@@ -2,6 +2,11 @@ import os
 import shutil
 from sklearn.model_selection import train_test_split
 
+def move_files(file_list, source_folder, destination_folder):
+    os.makedirs(destination_folder, exist_ok=True)
+    for file in file_list:
+        shutil.copy(os.path.join(source_folder, file), os.path.join(destination_folder, file))
+
 # Set the path to your dataset folder
 dataset_folder = '/content/drive/MyDrive/MoNuSeg/MoNuSegAnnotatedImages'
 
@@ -18,15 +23,13 @@ os.makedirs(test_folder, exist_ok=True)
 image_files = [f for f in os.listdir(dataset_folder) if f.endswith(".png")]
 
 # Split the dataset into training, validation, and testing sets
-train_files, test_files = train_test_split(image_files, test_size=0.2, random_state=42)
+train_files, test_val_files = train_test_split(image_files, test_size=0.2, random_state=42)
 train_files, val_files = train_test_split(train_files, test_size=0.1, random_state=42)
 
+# Further split the test set to reserve some tissue types
+val_files, test_files = train_test_split(test_val_files, test_size=0.5, random_state=42)
+
 # Move files to their respective folders
-for file in train_files:
-    shutil.copy(os.path.join(dataset_folder, file), os.path.join(train_folder, file))
-
-for file in val_files:
-    shutil.copy(os.path.join(dataset_folder, file), os.path.join(val_folder, file))
-
-for file in test_files:
-    shutil.copy(os.path.join(dataset_folder, file), os.path.join(test_folder, file))
+move_files(train_files, dataset_folder, train_folder)
+move_files(val_files, dataset_folder, val_folder)
+move_files(test_files, dataset_folder, test_folder)
